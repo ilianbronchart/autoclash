2d83b01b483503da9426ae5356f967bed79feac3
Yannick <yannick.vanneste1@gmail.com>
2023-10-24 22:38:26 +0200

    test added for detect button/screen

---
+++ test/detect_screen_test.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+import pytest
+
+from src.api.buttons import detect_button
+
+# Directory containing the test screenshots
+SCREENSHOTS_DIR = "./test/screenshots" 
+
+@pytest.mark.parametrize("./assets/templates/yannick.png", os.listdir(SCREENSHOTS_DIR))
+def test_detect_screen(screenshot_name):
+    # Load the screenshot
+    screenshot_path = os.path.join(SCREENSHOTS_DIR, screenshot_name)
+    screenshot = cv2.imread(screenshot_path)
+
+    # Use the detect_button function
+    detected_screen = detect_button(screenshot)
+
+    # Remove file extension from the screenshot_name to get expected screen name
+    expected_screen = os.path.splitext(screenshot_name)[0]
+
+    # Assert that detected screen matches the expected screen
+    assert detected_screen == expected_screen, f"Expected {expected_screen}, but got {detected_screen}"
